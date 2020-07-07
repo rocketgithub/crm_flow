04d2e349383ff34a1196dfa27c0ba62ad4e8b69a
jefferson <jeffer.leo93@gmail.com>
2020-07-07 16:34:49 -0600

    crm flow

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "CRM flow",
+
+    'summary': """ CRM flow """,
+
+    'description': """
+        CRM flow
+    """,
+
+    'author': "aquíH",
+    'website': "http://www.aquih.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['crm','mail'],
+
+    'data': [
+    ],
+    'qweb': [
+    ],
+}
---
+++ models/mail_activity.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from collections import defaultdict
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+import logging
+import pytz
+
+from odoo import api, exceptions, fields, models, _
+
+from odoo.tools.misc import clean_context
+from odoo.addons.base.models.ir_model import MODULE_UNINSTALL_FLAG
+
+_logger = logging.getLogger(__name__)
+
+
+class MailActivityType(models.Model):
+    _inherit = 'mail.activity.type'
+
+
+    delay_unit = fields.Selection(selection_add=[('horas', 'horas')])
