2032595fea037deada58811ea207b958c02f4214
jefferson <jeffer.leo93@gmail.com>
2020-11-30 11:25:12 -0600

    No importar librerias

--- models/crm_team.py
+++ models/crm_team.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from dateutil.relativedelta import relativedelta
-
 from odoo import api, fields, models, _
-from odoo.tools.safe_eval import safe_eval
-from odoo.exceptions import ValidationError
-
 
 class Team(models.Model):
     _inherit = 'crm.team'
